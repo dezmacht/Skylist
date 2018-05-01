5fc192416a005bc3107b68d76ef8c67eeed9c7b0
Carlos <dezmacht@gmail.com>
2018-05-01 11:24:40 -0400

    index.php added

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>
+ 		Skylist - Home
+ 	</title>
+ </head>
+ <body>
+<!--if !auth-->
+<!--navbar-->
+<!--content-->
+<!--footer-->
+<!--View for login shows-->
+
+<?php  ?>
+ <h1></h1>
+ </body>
+ </html>
